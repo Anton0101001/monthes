dd41de531a240542a3543be5aabbfc9b764fae24
anton shabanov <89005421555@mail.ru>
2022-04-15 15:47:35 +0300

    edit test

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -61,22 +61,5 @@ public class StatsService {
         return count;
     }
 
-
 }
-//        int january = 8;
-//        int february = 15;
-//        int march = 13;
-//        int april = 15;
-//        int may = 17;
-//        int june = 20;
-//        int july = 19;
-//        int august = 20;
-//        int september = 7;
-//        int october = 14;
-//        int november = 14;
-//        int dacember = 18;
-//
-//        int[] month = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-//
-
 
